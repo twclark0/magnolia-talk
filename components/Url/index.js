f378de85ad72075ede52d949aa7c6828ba99acf2
twclark0 <tylerc644@gmail.com>
2019-04-18 08:45:27 -0500

    Final Presentation

---
+++ components/Url/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Url = ({ text, ...props }) => (
+  <h1 style={{fontFamily: 'monospace'}}>
+      {text}
+  </h1>
+)
+
+export default Url
--- components/footer/index.js
+++ components/footer/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import './styles.css'
+
+const styles = {
+  position: 'absolute',
+    bottom: '0',
+    right: '25px',
+    fontSize: '30px',
+    color: '#f0f'
+}
 
 const Footer = () => (
   <div>
-    <p>Twiter: @iamtylerwclark</p>
-    <p>@egghead</p>
-    <p>@Pluralsight</p>
+    <p style={styles}>@iamtylerwclark</p>
   </div>
 )
 
